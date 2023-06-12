05deef75e26c0f7383ad2974ed900e5a5ecb1fb1
ShaneQin <664633146@qq.com>
2023-06-12 12:49:54 +0800

    fix: 修改asyncStorageGetS入参类型

--- packages/quickapp-plugins/dist/index.d.ts
+++ packages/quickapp-plugins/dist/index.d.ts
@@ -1,6 +1,6 @@
 declare class QuickAppPlugins {
     promiseFinally(): void;
 }
-declare const asyncStorageGetS: (key: string, dVal?: string) => Promise<any>;
+declare const asyncStorageGetS: (key: string, dVal?: any) => Promise<any>;
 
 export { asyncStorageGetS, QuickAppPlugins as default };
